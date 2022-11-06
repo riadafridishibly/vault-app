25649f18dcbed00a41b2191e2456da5ab683db22
ROOT\sabya <sabyasachis840@gmail.com>
2022-11-06 15:23:24 +0600

    Remove tabs.data.ts and reflect routes solely on routes.ts

---
+++ frontend/src/shared/enums/route-type.enum.ts
@@ -0,0 +1,7 @@
+// TODO: route type may change depending on authentication logic
+export enum RouteType {
+    Default = 'default',
+    Account = 'account',
+    General = 'general',
+    Hidden = 'hidden',
+}
