e8a044c2af2dc5498679026cf3def17ad9d5ee93
Riad Afridi Shibly <riadafridishibly@gmail.com>
2022-11-14 01:07:56 +0600

    Implement Password Manager UI

--- frontend/src/pages/PasswordManager/PasswordManager.tsx
+++ frontend/src/pages/PasswordManager/PasswordManager.tsx
@@ -1,12 +1,14 @@
 import DataTable from '@src/components/DataTable/DataTable';
 import CreatePassword from '@src/shared/components/CreatePassword';
 import { passwordDummy } from '@src/shared/data/dummy-data.data';
+import { data, PasswordManagerTable } from './Table';
 
+// <DataTable tableData={passwordDummy}></DataTable>
 function PasswordManager() {
     return (
         <>
             <CreatePassword />
-            <DataTable tableData={passwordDummy}></DataTable>
+            <PasswordManagerTable data={data.data} />
         </>
     );
 }
