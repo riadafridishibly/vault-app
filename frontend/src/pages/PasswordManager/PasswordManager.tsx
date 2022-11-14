ce1a013cfe4345635fe5022474423a32fd95ba56
Riad Afridi Shibly <riadafridishibly@gmail.com>
2022-11-15 01:46:42 +0600

    Add password manager data entry functionalities

--- frontend/src/pages/PasswordManager/PasswordManager.tsx
+++ frontend/src/pages/PasswordManager/PasswordManager.tsx
@@ -1,14 +1,12 @@
-import DataTable from '@src/components/DataTable/DataTable';
-import CreatePassword from '@src/shared/components/CreatePassword';
-import { passwordDummy } from '@src/shared/data/dummy-data.data';
-import { data, PasswordManagerTable } from './Table';
+import { CreatePasswordItemForm } from "@src/shared/components/CreatePassword";
+import { data, PasswordManagerTable } from "./Table";
 
 // <DataTable tableData={passwordDummy}></DataTable>
 function PasswordManager() {
     return (
         <>
-            <CreatePassword />
-            <PasswordManagerTable data={data.data} />
+            <CreatePasswordItemForm />
+            <PasswordManagerTable />
         </>
     );
 }
--- frontend/src/pages/PasswordManager/Table.tsx
+++ frontend/src/pages/PasswordManager/Table.tsx
@@ -7,7 +7,7 @@ import {
     IconShare,
     IconAlertOctagon,
 } from '@tabler/icons';
-import { atom } from 'recoil';
+import { atom, useRecoilValue } from 'recoil';
 import { ent } from '@wailsjs/go/models'
 
 import { useHover } from '@mantine/hooks';
@@ -27,70 +27,65 @@ function PasswordField({ password }: { password: string | undefined; }) {
 }
 
 
-interface PasswordItems {
-    data: Array<ent.PasswordItem>;
-}
+export const data: ent.PasswordItem[] = [
+    new ent.PasswordItem({
+        "id": 1,
+        "avatar": "https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+        "site_name": "Robert Wolfkisser",
+        "site_url": "https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+        "username": "rob_wolf@gmail.com",
+        "password": "1234",
+    }),
+    new ent.PasswordItem({
+        "id": 2,
+        "avatar": "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+        "site_name": "Jill Jailbreaker",
+        "site_url": "Engineer",
+        "username": "jj@breaker.com",
+        "password": "1234",
 
-export const data: PasswordItems = {
-    "data": [
-        new ent.PasswordItem({
-            "id": 1,
-            "avatar": "https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
-            "site_name": "Robert Wolfkisser",
-            "site_url": "https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
-            "username": "rob_wolf@gmail.com",
-            "password": "1234",
-        }),
-        new ent.PasswordItem({
-            "id": 2,
-            "avatar": "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
-            "site_name": "Jill Jailbreaker",
-            "site_url": "Engineer",
-            "username": "jj@breaker.com",
-            "password": "1234",
+    }),
+    new ent.PasswordItem({
+        "id": 3,
+        "avatar": "https://images.unsplash.com/photo-1632922267756-9b71242b1592?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+        "site_name": "Henry Silkeater",
+        "site_url": "Designer",
+        "username": "henry@silkeater.io",
+        "password": "1234",
+    }),
+    new ent.PasswordItem({
+        "id": 4,
+        "avatar": "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+        "site_name": "Bill Horsefighter",
+        "site_url": "Designer",
+        "username": "bhorsefighter@gmail.com",
+        "password": "1234",
+    }),
+    new ent.PasswordItem({
+        "id": 5,
+        "avatar": "https://images.unsplash.com/photo-1630841539293-bd20634c5d72?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+        "site_name": "Jeremy Footviewer",
+        "site_url": "Manager",
+        "username": "jeremy@foot.dev",
+        "password": "1234",
+    }),
+    new ent.PasswordItem({
+        "id": 6,
+        "avatar": "https://images.unsplash.com/photo-1630841539293-bd20634c5d72?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+        "site_name": "Jeremy Footviewer",
+        "site_url": "Manager",
+        "email": "jeremy@foot.dev",
+        "password": "1234",
+    })
+]
 
-        }),
-        new ent.PasswordItem({
-            "id": 3,
-            "avatar": "https://images.unsplash.com/photo-1632922267756-9b71242b1592?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
-            "site_name": "Henry Silkeater",
-            "site_url": "Designer",
-            "username": "henry@silkeater.io",
-            "password": "1234",
-        }),
-        new ent.PasswordItem({
-            "id": 4,
-            "avatar": "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
-            "site_name": "Bill Horsefighter",
-            "site_url": "Designer",
-            "username": "bhorsefighter@gmail.com",
-            "password": "1234",
-        }),
-        new ent.PasswordItem({
-            "id": 5,
-            "avatar": "https://images.unsplash.com/photo-1630841539293-bd20634c5d72?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
-            "site_name": "Jeremy Footviewer",
-            "site_url": "Manager",
-            "username": "jeremy@foot.dev",
-            "password": "1234",
-        }),
-        new ent.PasswordItem({
-            "id": 6,
-            "avatar": "https://images.unsplash.com/photo-1630841539293-bd20634c5d72?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
-            "site_name": "Jeremy Footviewer",
-            "site_url": "Manager",
-            "email": "jeremy@foot.dev",
-            "password": "1234",
-        })
-    ]
-}
-
-const passwordMangerItems = atom<PasswordItems>({
+export const passwordMangerItems = atom<ent.PasswordItem[]>({
     key: 'passwordMangerItems',
     default: data,
 })
 
-export function PasswordManagerTable({ data }: PasswordItems) {
+export function PasswordManagerTable() {
+    const data = useRecoilValue(passwordMangerItems)
     const { copy } = useGoClipboard()
     const rows = data.map((item) => (
         <tr key={item.id}>
