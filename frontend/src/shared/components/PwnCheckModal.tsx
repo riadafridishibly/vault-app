2a698f186091b7ed0bda2bfb7fb496a34fbb1c33
Riad <riadafridishibly@gmail.com>
2022-11-22 01:49:15 +0600

    Add loader in pwn checker

--- frontend/src/shared/components/PwnCheckModal.tsx
+++ frontend/src/shared/components/PwnCheckModal.tsx
@@ -1,47 +1,46 @@
-import { Button, Group, Modal, Text } from '@mantine/core';
+import { Button, Group, Loader, LoadingOverlay, Modal, Text } from '@mantine/core';
 import { CheckPasswordBreach } from '@wailsjs/go/main/PasswordService';
-import { group } from 'console';
-import React, { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { atom, useRecoilState } from 'recoil';
 import { modalButtonColor, modalButtonMargin, modalButtonSize } from '../constants/modal.constants';
 
-let color = 'green';
-let innerText = '';
-
-export const showPasswordBreachModal = atom<{ opened: boolean; password: string; count: number }>({
+export const showPasswordBreachModal = atom<{
+    opened: boolean;
+    password: string;
+}>({
     key: 'showPasswordBreachModal',
     default: {
         opened: false,
         password: '',
-        count: -1,
     },
-    effects: [
-        ({ setSelf, onSet }) => {
-            onSet(async (newValue) => {
-                let count = 0;
-                try {
-                    count = await CheckPasswordBreach(newValue.password);
-                } catch (ex) {
-                    console.log(ex);
-                } finally {
-                    setSelf({ ...newValue, count: count });
-                    console.log(count, color);
-                }
-                color = count > 0 ? 'red' : 'green';
-                innerText =
-                    count !== -1
-                        ? count > 0
-                            ? `Password breach found in ${count} instances!`
-                            : `No password breach found`
-                        : ``;
-            });
-        },
-    ],
 });
 
 const PwnCheckModal = () => {
     const [passwordBreachModalState, setPasswordBreachModalState] =
         useRecoilState(showPasswordBreachModal);
+    const [color, setColor] = useState<'green' | 'red'>('green');
+    const [loading, setLoading] = useState(false);
+    const [msg, setMsg] = useState('No password breach found');
+
+    const apiCallFn = useCallback(async () => {
+        let count = 0;
+        try {
+            setLoading(true);
+            count = await CheckPasswordBreach(passwordBreachModalState.password);
+        } catch (ex) {
+            console.error(ex);
+        } finally {
+            setLoading(false);
+            setColor(count > 0 ? 'red' : 'green');
+            if (count > 0) {
+                setMsg(`Password breach found in ${count} instances!`);
+            }
+        }
+    }, [passwordBreachModalState.password]);
+
+    useEffect(() => {
+        apiCallFn();
+    }, [passwordBreachModalState.opened]);
 
     return (
         <>
@@ -49,28 +48,37 @@ const PwnCheckModal = () => {
                 centered={true}
                 opened={passwordBreachModalState.opened}
                 onClose={() =>
-                    setPasswordBreachModalState({ opened: false, password: '', count: -1 })
+                    setPasswordBreachModalState({
+                        opened: false,
+                        password: '',
+                    })
                 }
             >
-                <Text color={color} align="center">
-                    {passwordBreachModalState.count > 0
-                        ? `Password breach found in ${passwordBreachModalState.count} instances!`
-                        : passwordBreachModalState.count !== -1
-                        ? `No password breach found`
-                        : ``}
-                </Text>
-                <Group position="center">
-                    <Button
-                        size={modalButtonSize}
-                        m={modalButtonMargin}
-                        bg={modalButtonColor}
-                        onClick={() =>
-                            setPasswordBreachModalState({ opened: false, password: '', count: -1 })
-                        }
-                    >
-                        OK
-                    </Button>
-                </Group>
+                <div>
+                    <LoadingOverlay
+                        loader={<Loader size="lg" variant="dots" />}
+                        overlayOpacity={1}
+                        visible={loading}
+                    />
+                    <Text color={color} align="center">
+                        {msg}
+                    </Text>
+                    <Group position="center">
+                        <Button
+                            size={modalButtonSize}
+                            m={modalButtonMargin}
+                            bg={modalButtonColor}
+                            onClick={() =>
+                                setPasswordBreachModalState({
+                                    opened: false,
+                                    password: '',
+                                })
+                            }
+                        >
+                            OK
+                        </Button>
+                    </Group>
+                </div>
             </Modal>
         </>
     );
